bf75a193201301393d039af9128bccf17ae5de93
rood0039 <rood0039@umn.edu>
2024-04-15 12:40:19 -0500

    add  comments and hinting to ResourcePrefix()

--- Singularity.py
+++ Singularity.py
@@ -56,7 +56,14 @@ PrevScans = []
 ALPHA_BEGIN = 97
 ALPHA_END = 124
 
-def ResourcePrefix():
+def ResourcePrefix() -> str:
+    """
+    The function `ResourcePrefix` returns specific resource path prefixes based on if the game
+    is in a dev or built state.
+    @returns The function `ResourcePrefix()` is returning a string value. The returned value depends on
+    whether the directory `_internal` exists. If the directory `_internal` exists, the function will
+    return `"_internal/assets/"`, otherwise it will return `"assets/"`.
+    """
     if os.path.isdir('_internal'):
         return "_internal/assets/"
     else:
