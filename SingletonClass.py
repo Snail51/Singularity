c8e82a4beb21a7f20165ed2e269fcf98caac9148
mobren11 <mobren11@gmail.com>
2025-02-12 14:47:48 -0600

    add install and cfg to README
    - Proper install instructions now in README.md
    - Info about game configuration now in README.md
    - Updated internal README.md
    - Removed unused `SingletonClass.py`

--- SingletonClass.py
+++
@@ -1,8 +0,0 @@
-
-class SingletonClass:
-    def __new__(cls, *args, **kwargs):
-        if not hasattr(cls, 'instance'):
-            cls.instance = super(SingletonClass, cls).\
-                __new__(cls, args, kwargs)
-        return cls.instance
-    
